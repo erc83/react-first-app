8900d728efebcb6b0fe355ad32373e1376fbb78f
eric-espinoza <espinoza.eric@gmail.com>
2021-04-14 02:40:35 -0400

    agregado ruta dinamica a topics.js

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -1,7 +1,35 @@
+import {
+    Link,
+    Route,
+  } from 'react-router-dom'
+
+
+
+
+function Topic ({ match }) {
+    return <h3>{match.params.topicId}</h3>
+}
+
+ 
 function Topics () {
     return (
       <div>
-        Hello Topics
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to='/topics/rendering'>Rendering with React</Link>
+            </li>
+            <li>
+                <Link to='/topics/components'>Components</Link>
+            </li>
+            <li>
+                <Link to='/topics/props-v-state'>Props vs State</Link>
+            </li>
+        </ul>
+        <hr />
+        <Route path='/topics/:topicId' component={Topic} />
+
+
       </div>
     );
   }
