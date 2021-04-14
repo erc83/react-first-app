503c0e888cd143360681cea19c5240dc01c86734
eric-espinoza <espinoza.eric@gmail.com>
2021-04-13 21:19:27 -0400

    dia4 configuracion inicial y exportacion de Home a App

---
+++ intro/src/components/App.js
@@ -0,0 +1,12 @@
+import Home from './Home'
+
+function App() {
+  return (
+    <div>
+      <Home />
+      Hello Eric
+    </div>
+  );
+}
+
+export default App
---
+++ intro/src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        Hello Home
+      </div>
+    );
+  }
+  
+  export default Home
+  
