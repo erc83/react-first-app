66faa87d031c5edaddc080dc625f7b7a59771a4d
eric-espinoza <espinoza.eric@gmail.com>
2021-04-14 01:54:50 -0400

    agregando Route app.js

--- intro/src/components/App.js
+++ intro/src/components/App.js
@@ -17,6 +17,12 @@ function App() {
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/topics'>Topics</Link></li>
         </ul>
+        <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/topics' component={Topics} />
+
       </div>
     </BrowserRouter>
   );
